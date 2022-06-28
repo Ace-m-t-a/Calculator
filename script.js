87c332adb486e8bb07ff6ef5906ac25db5e8420d
Mueen <mueentaj1998@outlook.com>
2022-06-28 13:30:03 +0100

    Edit index.html

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const negativeBtn = document.querySelector('.negativeBtn');
+const display = document.querySelector('.display');
+const clearBtn = document.querySelector('.clearBtn');
+const addBtn = document.querySelector('.addBtn');
+const multiplyBtn = document.querySelector('.multiplyBtn');
+const divideBtn = document.querySelector('.divideBtn');
+const subtractBtn = document.querySelector('.subtractBtn');
+const equalsBtn = document.querySelector('.equalsBtn');
+const decimalBtn = document.querySelector('.decimalBtn');
+const numButtons = document.querySelectorAll('numButtons');
+const buttons = document.querySelectorAll('button');
+let isClicked = false;
+
+display.textContent = "";
+let firstOperand = "";
+let secondOperand = "";
+let operator = "";
+let prevResult = "";
+
+
+
 function add(a, b){
     return a + b
 }
@@ -18,45 +39,62 @@ function divide(a, b){
 }
 
 function operate(operator, num1, num2){
+    let numericNum1 = parseInt(num1)
+    let numericNum2 = parseInt(num2)
     switch(operator){
         case '+':
-            return add(num1,num2);
+            return add(numericNum1,numericNum2).toFixed(2);
         case '-':
-            return subtract(num1, num2);
+            return subtract(numericNum1, numericNum2).toFixed(2);
         case '*':
-            return multiply(num1, num2);
+            return multiply(numericNum1, numericNum2).toFixed(2);
         case '/':
-            return divide(num1, num2);
+            return divide(numericNum1, numericNum2).toFixed(2);
     }
 }
-console.log(operate('+', 4, 2))
-console.log(operate('-', 4, 2))
-console.log(operate('*', 4, 2))
-console.log(operate('/', 4, 2))
-console.log(operate('/', 0, 2))
-console.log(operate('/', 2, 0))
 
-const buttons = document.querySelectorAll('button');
-const display = document.querySelector('.display');
-display.textContent = "0";
-let values = {
-    firstNum: "",
-    operator: "",
-    secondNum:""
+function buttonClicks(e){
+    value = e.target.className
+    if(value == "numButton" && operator.length != 1){
+        firstOperand += e.target.value
+        display.textContent = firstOperand
+    } else if((value == "addBtn" ||value == "subtractBtn" ||value == "divideBtn" ||value == "multiplyBtn") && containsNumber(firstOperand)){
+        if(operator.length == 1){
+            return
+        } else {
+            operator = e.target.value
+        }
+        display.textContent = operator
+    } else if(value == "numButton" && containsNumber(firstOperand) && operator.length == 1){
+        secondOperand += e.target.value
+        display.textContent = secondOperand
+    } else if (value == "equalBtn" && containsNumber(secondOperand)){
+        isClicked = true
+        isEqualPressed()
+    }
+}
+
+function isEqualPressed(buttonClicks){
+    prevResult = operate(operator, firstOperand, secondOperand)
+    display.textContent = prevResult
+    operator = ""
+    secondOperand = ""
+    if(isClicked){
+        firstOperand = prevResult
+    }
 }
 
 buttons.forEach((btn) => {
-    btn.addEventListener('click', storeValue);
+    btn.addEventListener('click', buttonClicks);
     btn.addEventListener('mouseover', changeColor);
     btn.addEventListener('mouseleave', returnColor);
 })
 
 
-function storeValue(e){
-        let newValue = e.target.value
-        display.textContent += newValue
 
-}
+function containsNumber(str) {
+    return /\d/.test(str);
+  }
 
 function changeColor(e){
     e.target.style.backgroundColor = 'white';
